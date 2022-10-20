d46ffb36f7c7bc22773969ab26ad1c9a56d5340f
reshma-dev <reshma.sahasrabudhe@gmail.com>
2022-10-20 10:38:48 +0100

    Checks that the input is a single character

--- milestone_2.py
+++ milestone_2.py
@@ -7,3 +7,10 @@ word = random.choice(word_list)
 print("Random word: ", word)
 
 guess = input("Enter a single letter: ")
+
+# A string is alphabetic if all characters in the string are alphabetic 
+# and there is at least one character in the string.
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")

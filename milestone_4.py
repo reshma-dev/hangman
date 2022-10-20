055a322434d2dde73cdc35d85e191df74ee1f842
reshma-dev <reshma.sahasrabudhe@gmail.com>
2022-10-20 16:39:20 +0100

    Defines what happens when guess is matched

--- milestone_4.py
+++ milestone_4.py
@@ -3,7 +3,7 @@ import random
 class Hangman():
     def __init__(self, word_list, num_lives = 5):
         self.word = random.choice(word_list)
-        self.word_guessed = [''] * len(self.word)
+        self.word_guessed = ['_'] * len(self.word)
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
@@ -14,12 +14,17 @@ class Hangman():
     
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
+            for idx, letter in enumerate(self.word):
+                if letter == guess:
+                    self.word_guessed[idx] = guess
+            self.num_letters -= 1
         else:
             print(f"Sorry, {guess} is not in the word. Try again.")
 
     def ask_for_input(self):
         while True:
             guess = input("Guess a letter: ")
+            
             if len(guess) != 1 or not guess.isalpha():
                 print("Invalid letter. Please, enter a single alphabetical character.")
             elif guess in self.list_of_guesses:

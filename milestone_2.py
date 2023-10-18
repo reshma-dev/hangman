0dd0f2406a5c6d517752fc7e99dd803550aaadf4
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-10-18 16:35:40 +0100

    add a function to choose a random word from list

--- milestone_2.py
+++ milestone_2.py
@@ -1,2 +1,12 @@
-word_list = ['apple', 'mango', 'cherry', 'blueberry', 'strawberry']
-print(word_list)
+import random
+
+
+def choose_word():
+    word_list = ["apple", "mango", "cherry", "blueberry", "strawberry"]
+    return random.choice(word_list)
+
+
+if __name__ == "__main__":
+    for i in range(10):
+        word = choose_word()
+        print(word)

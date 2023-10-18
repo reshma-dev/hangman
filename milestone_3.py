5aa3e1cc0a0f3203871352267d4bdd148c96cecb
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-10-18 21:40:05 +0100

    refactor: create a function for checking guess

--- milestone_3.py
+++ milestone_3.py
@@ -6,22 +6,29 @@ def choose_word():
     return random.choice(word_list)
 
 
-def guess_letter():
+def ask_for_input():
     while True:
-        guess = input("Enter a single letter: ").strip()
-        if len(guess) == 1 and guess.isalpha():
-            return guess
+        letter = input("Enter a single letter: ").strip()
+        if len(letter) == 1 and letter.isalpha():
+            return letter
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
+
+
+def check_guess(word, letter):
+    letter = letter.lower()
+    word = word.lower()
+
+    if word.find(letter) >= 0:
+        print(f"Good guess! '{letter}' is in the word.")
+    else:
+        print(f"Sorry, {letter} is not in the word. Try again")
 
 
 if __name__ == "__main__":
     # Check whether the guess is in the word
     word = choose_word()
-    guess = guess_letter()
-    print(f"Word: {word}, guess: {guess}")
+    guess = ask_for_input()
 
-    if word.lower().find(guess.lower()) >= 0:
-        print(f"Good guess! '{guess}' is in the word.")
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again")
+    check_guess(word, guess)
+    print(f"Word: {word}, guess: {guess}")

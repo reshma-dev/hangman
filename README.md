97a735ffc21d3884bdaee2138b93dde725b14a34
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-10-18 21:26:39 +0100

    add logic to check if guess is in the word

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# hangman
+# Implement the hangman game
 This is an implementation of the Hangman game, where the computer thinks of a word and the user tries to guess it.
+The purpose of this project is to gain experience using Python. It's developed in stages
+
+## TODO: Update ReadMe to include 
+Project Title
+Table of Contents, if the README file is long
+A description of the project: what it does, the aim of the project, and what you learned
+Installation instructions
+Usage instructions
+File structure of the project
+License information
+
--- milestone_3.py
+++ milestone_3.py
@@ -1,14 +1,27 @@
+import random
+
+
+def choose_word():
+    word_list = ["apple", "mango", "cherry", "blueberry", "strawberry"]
+    return random.choice(word_list)
+
+
 def guess_letter():
     while True:
         guess = input("Enter a single letter: ").strip()
         if len(guess) == 1 and guess.isalpha():
-            print("Good guess!")
             return guess
         else:
             print("Invalid letter. Please, enter a single alphabetical character.")
 
 
 if __name__ == "__main__":
-    # Test one letter guess request from user
+    # Check whether the guess is in the word
+    word = choose_word()
     guess = guess_letter()
-    print(guess)
+    print(f"Word: {word}, guess: {guess}")
+
+    if word.lower().find(guess.lower()) >= 0:
+        print(f"Good guess! '{guess}' is in the word.")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again")

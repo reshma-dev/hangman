f52fc04c0f6bc810cbb7ef9fb6bd14a7c8d55378
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-10-18 16:49:41 +0100

    add function to request a letter from the user

--- milestone_2.py
+++ milestone_2.py
@@ -6,7 +6,22 @@ def choose_word():
     return random.choice(word_list)
 
 
+def guess_letter():
+    while True:
+        guess = input("Enter a single letter: ").strip()
+        if len(guess) == 1 and guess.isalpha():
+            print("Good guess!")
+            return guess
+        else:
+            print("Oops! This is not a valid input, try again...")
+
+
 if __name__ == "__main__":
-    for i in range(10):
-        word = choose_word()
-        print(word)
+    # Test random choice of word from list
+    # for i in range(10):
+    #     word = choose_word()
+    #     print(word)
+
+    # Test one letter guess request from user
+    guess = guess_letter()
+    print(guess)

2510da95664956fd3a01d9bdd37acad56d0d3e8f
reshma-dev <reshma.sahasrabudhe@gmail.com>
2023-10-19 19:37:14 +0100

    add docstrings bugfix: for condition where letter is guessed correctly

--- milestone_5.py
+++ milestone_5.py
@@ -1,9 +1,53 @@
 import random
 
-# TODO: Add Docstring
-
 
 class Hangman:
+    """
+    Hangman is a classic game in which a player thinks of a word
+    and the other player tries to guess that word within a certain amount of attempts.
+
+    This is an implementation of the game, where the computer thinks of a word
+    and the user tries to guess it.
+
+    The class uses the `random` module to pick a word from the word_list
+
+    Parameters:
+    ----------
+    word_list: list
+        List of words from which a word is randomly chosen by the program
+    num_lives: int
+        Number of lives the player has at the start of the game.
+        Optional argument, defaults to 5 if not specified
+
+    Attributes:
+    ----------
+    __word: str
+        The word to be guessed, picked randomly from the word_list
+    __word_guessed: list
+        A list of the letters of the word, with '_' for each letter not yet guessed
+    __num_letters_not_guessed: int
+        The number of UNIQUE letters in the word that have not been guessed yet
+    __num_lives: int
+        The number of lives the player has
+    __list_of_guesses: list
+        A list of the guesses that have already been tried
+    __word_list: list
+        List of words used to pick a random word for guessing
+
+
+    Methods:
+    -------
+    play_game()
+        The only public method to be called to play the game with a word list
+        This method puts the game logic together.
+    __check_guess(letter)
+        Checks if the letter is in the word.
+    __ask_for_input()
+        Asks the user for a guess.
+        Continues to request for a letter until a valid, single alphabet character is entered.
+
+    """
+
     def __init__(self, word_list, num_lives=5) -> None:
         self.__word_list = word_list
         self.__num_lives = num_lives
@@ -11,12 +55,19 @@ class Hangman:
         self.__word = random.choice(word_list).lower()
         self.__word_guessed = list("_" * len(self.__word))
 
-        self.__num_letters_not_guessed = len(
-            set(self.__word) - set(str(self.__word_guessed))
-        )
+        self.__num_letters_not_guessed = len(set(self.__word))
         self.__list_of_guesses = []
 
     def __ask_for_input(self):
+        """
+        Ask user to guess one letter
+
+        Validate that a single character is entered
+        Validate that the entered letter is an alphabetical character
+
+        If an already guessed letter is entered again, notify the user
+        If a new guess is entered, call check_guess and add letter to guessed list
+        """
         while True:
             letter = input("Enter a single letter: ").strip()
             if not len(letter) == 1 or not letter.isalpha():
@@ -29,6 +80,17 @@ class Hangman:
                 break
 
     def __check_guess(self, letter):
+        """
+        Checks if the letter is in the randomly chosen word.
+        If it is, the corresponding '_' in the word_guessed list is replaced with the letter.
+        If it is not, number of remaining lives is reduced by 1.
+
+        Parameters:
+        ----------
+        letter: str
+            The letter to be checked as a guess
+
+        """
         letter = letter.lower()
 
         if self.__word.find(letter) >= 0:
@@ -37,12 +99,26 @@ class Hangman:
                 if character == letter:
                     self.__word_guessed[i] = letter
             print(self.__word_guessed)
+            self.__num_letters_not_guessed -= 1
         else:
             self.__num_lives -= 1
             print(f"Sorry, '{letter}' is not in the word")
             print(f"You have {self.__num_lives} lives left")
 
     def play_game(self):
+        """
+        Brings the game logic together
+
+        Check if num_lives is 0.
+        If it is, that means the game has ended and the user lost.
+
+        Check if num_letters_not_guessed > 0.
+        If it is, then to continue the game, call ask_for_input method.
+
+        If the num_lives is not 0 and the num_letters_not_guessed is not greater than 0,
+        that means the user has won the game.
+        """
+        print(self.__word_guessed)
         while True:
             if self.__num_lives == 0:
                 print("You lost!")
